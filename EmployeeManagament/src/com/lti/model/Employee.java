f1bf47c868d4260678fed17aa980a5c06d70ee9c
Shubham Tripathi <45036978+realtripathi@users.noreply.github.com>
2020-10-15 12:42:49 +0530

    second commit

---
+++ EmployeeManagament/src/com/lti/model/Employee.java
@@ -0,0 +1,30 @@
+package com.lti.model;
+
+public class Employee {
+	private int employeeId;
+	private String employeeName;
+	
+	
+
+	public Employee() {
+		//TODO 
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	
+
+}
